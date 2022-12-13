859b4ae3055d451ec5010bb629ca6ad282948ef4
Ederson de Jesus <edersonjseder@gmail.com>
2022-12-12 19:36:50 -0500

    modifying script to node server

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://pinvent-app-one.vercel.app"],
+    origin: ["http://localhost:3000", "https://pinvent-frontend.vercel.app"],
     credentials: true,
   })
 );
